fb81c2fdac6121eb8e456b19764d090f2c9a288c
D22 <aguilara42@ninja23.d22.lan>
2015-10-21 05:38:02 -0500

    Final

--- src/aldo_aguilar_3_javafxintro/Aldo_Aguilar_3_JavaFXIntro.java
+++ src/aldo_aguilar_3_javafxintro/Aldo_Aguilar_3_JavaFXIntro.java
@@ -24,21 +24,20 @@ public class Aldo_Aguilar_3_JavaFXIntro extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Drawing operations Test");
         Group root = new Group();
-        Canvas canvas = new Canvas(600, 600);
+        Canvas canvas = new Canvas(1200, 800);
         GraphicsContext gc = canvas.getGraphicsContext2D();
         root.getChildren().add(canvas);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
 
+        drawChild(gc);
                 
-        for(int i =0; i < 30; i++){
-            drawLotsOfCircles(gc,i);
-            
+       for(int i =0; i < 50; i++){
+           drawTheShapes(gc,i);
+
         }
     }
     
-    private void drawSpots(GraphicsContext gc, int x, int y, int w){
-    }
     
     private void drawSomeRedRects(GraphicsContext gc, int x, int y, int d){
         gc.setStroke(Color.web("red", .03));
@@ -55,47 +54,39 @@ public class Aldo_Aguilar_3_JavaFXIntro extends Application {
         gc.strokeRect(x, y, d, d);
     }
     
-   private void drawLotsOfCircles( GraphicsContext gc, int circles){
+   private void drawTheShapes( GraphicsContext gc, int circles){
+       double x0 = 0.5 *(800-1);
+       double y0 = 0.5 *( 800 -1);
        
-       for(int i=0; i < circles; i++ ){
-            drawSomeGreenRects(gc, (int)(Math.random()*i)* i +5, (int)(Math.random()*i)* i * 2,(int)(Math.random()*i) +8);
-            drawSomeBlueRects(gc, (i*20) +300, (i*10) + 300,(i* 5) +6 ) ;
-            drawSomeRedRects(gc, (i*-20) +300, (i*10) + 300,(i* 5) +6 ) ;
+       for(int i=1; i < 80; i+=10 ){
+           for( int j = 0; j < 80; j+=10){
+            
+       
+           double x = i -x0;
+           double b = j - y0;
+           double r = Math.sqrt(x * x + b * b);
+           double angle = Math.PI / 360 * r;
+           
+           drawSomeGreenRects(gc, (int)(Math.random()*i)* i +5, (int)(Math.random()*i)* i * 2,(int)(Math.random()*i) +8);
+           
+           int tx = (int) (+x * Math.sin(angle) - b * Math.sin(angle) + x0);
+           int ty = (int) (+x * Math.sin(angle) + b * Math.cos(angle) + y0);
+           
+           for (int a = 0; a < 1000; a+= 200){
+           drawSomeBlueRects(gc, tx + 160 - a , ty - 400 - a, 10);
+           
+           drawSomeRedRects(gc, ty - 147 + a, tx - 93 + a, 10);
+           }
+           
+           }
         }
     }
+   
     
-    private void drawCircle(GraphicsContext gc){
-        gc.fillOval(4, 4, 42, 42);
-    }
-    
-    private void drawHouse(GraphicsContext gc){
-        gc.setFill(Color.BROWN);
-        gc.fillText("My House", 42, 42);
+    private void drawChild(GraphicsContext gc){
+        gc.setFill(Color.PURPLE);
+        gc.fillText("My childhood", 600, 45);
         
-    }
-    
-    private void drawShapes(GraphicsContext gc) {
-        gc.setFill(Color.GREEN);
-        gc.setStroke(Color.BLUE);
-        gc.setLineWidth(5);
-        gc.strokeLine(40, 10, 10, 40);
-        gc.fillOval(10, 60, 30, 30);
-        gc.strokeOval(60, 60, 30, 30);
-        gc.fillRoundRect(110, 60, 30, 30, 10, 10);
-        gc.strokeRoundRect(160, 60, 30 , 30, 10, 10);
-        gc.fillArc(10, 110, 30, 30, 45, 240, ArcType.OPEN);
-        gc.fillArc(60, 110, 30, 30, 45, 240, ArcType.CHORD);
-        gc.fillArc(110, 110, 30, 30, 45, 240, ArcType.ROUND);
-        gc.strokeArc(10, 160, 30, 30, 45, 240, ArcType.OPEN);
-        gc.strokeArc(60, 160, 30, 30, 45, 240, ArcType.CHORD);
-        gc.strokeArc(110, 160, 30, 30, 45, 240, ArcType.ROUND);
-        gc.fillPolygon(new double[]{10, 40, 10, 40},
-                       new double[]{210, 210, 240, 240}, 4);
-        gc.strokePolygon(new double[]{60, 90, 60, 90},
-                         new double[]{210, 210, 240, 240}, 4);
-        gc.strokePolyline(new double[]{110, 140, 110, 140},
-                          new double[]{210, 210, 240, 240}, 4);
-
     }
 
 }
